5a0dfa9644ea04260a9174ae22feacdf85698870
Jerry Lee <oldratlee@gmail.com>
2023-03-13 20:14:17 +0800

    test/refactor: use `val by lazy` instead of `lateinit var`

--- src/test/java/io/foldright/testutils/TestThreadPoolManager.kt
+++ src/test/java/io/foldright/testutils/TestThreadPoolManager.kt
@@ -6,7 +6,7 @@ import io.kotest.assertions.fail
 import io.kotest.core.annotation.AutoScan
 import io.kotest.core.listeners.AfterProjectListener
 import io.kotest.core.listeners.BeforeProjectListener
-import io.kotest.matchers.shouldBe
+import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.string.shouldNotStartWith
 import io.kotest.matchers.string.shouldStartWith
 import java.lang.Thread.currentThread
@@ -16,13 +16,35 @@ import kotlin.random.Random
 import kotlin.random.nextULong
 
 
-lateinit var testThreadPoolExecutor: ExecutorService
-lateinit var testForkJoinPoolExecutor: ExecutorService
-
 const val THREAD_COUNT_OF_POOL = 10
 
 private val threadNamePrefixOfThreadPoolExecutor = "CompletableFutureUseTest_ThreadPool_${Random.nextULong()}-"
 private val threadNamePrefixOfForkJoinPoolExecutor = "CompletableFutureUseTest_ForkJoinPool_${Random.nextULong()}-"
+
+val testThreadPoolExecutor: ExecutorService by lazy {
+    val counter = AtomicLong()
+    ThreadPoolExecutor(
+        THREAD_COUNT_OF_POOL, THREAD_COUNT_OF_POOL, 1, TimeUnit.DAYS,
+        ArrayBlockingQueue(THREAD_COUNT_OF_POOL * 2)
+    ) { r ->
+        Thread(r).apply {
+            name = "$threadNamePrefixOfThreadPoolExecutor${counter.getAndIncrement()}"
+            isDaemon = true
+        }
+    }
+}
+
+val testForkJoinPoolExecutor: ExecutorService by lazy {
+    val counter = AtomicLong()
+    ForkJoinPool(
+        THREAD_COUNT_OF_POOL, { fjPool ->
+            ForkJoinPool.defaultForkJoinWorkerThreadFactory.newThread(fjPool).apply {
+                name = "$threadNamePrefixOfForkJoinPoolExecutor${counter.getAndIncrement()}"
+            }
+        },
+        null, false
+    )
+}
 
 fun assertRunInTestThreadPoolExecutor() {
     currentThread().name shouldStartWith threadNamePrefixOfThreadPoolExecutor
@@ -44,7 +66,7 @@ fun assertRunInExecutor(executor: Executor) {
     when (executor) {
         testThreadPoolExecutor -> assertRunInTestThreadPoolExecutor()
         testForkJoinPoolExecutor -> assertRunInTestForkJoinPoolExecutor()
-        else -> fail("should be happened")
+        else -> fail("should NOT happen")
     }
 }
 
@@ -52,7 +74,7 @@ fun assertRunNotInExecutor(executor: Executor) {
     when (executor) {
         testThreadPoolExecutor -> assertRunNotInTestThreadPoolExecutor()
         testForkJoinPoolExecutor -> assertRunNotInTestForkJoinPoolExecutor()
-        else -> fail("should be happened")
+        else -> fail("should NOT happen")
     }
 }
 
@@ -66,30 +88,6 @@ fun assertRunNotInExecutor(executor: Executor) {
 @AutoScan
 object InitTestThreadPoolsProjectListener : BeforeProjectListener, AfterProjectListener {
     override suspend fun beforeProject() {
-        testThreadPoolExecutor = run {
-            val counter = AtomicLong()
-            ThreadPoolExecutor(
-                THREAD_COUNT_OF_POOL, THREAD_COUNT_OF_POOL, 1, TimeUnit.DAYS,
-                ArrayBlockingQueue(THREAD_COUNT_OF_POOL * 2)
-            ) { r ->
-                Thread(r).apply {
-                    name = "$threadNamePrefixOfThreadPoolExecutor${counter.getAndIncrement()}"
-                    isDaemon = true
-                }
-            }
-        }
-        testForkJoinPoolExecutor = run {
-            val counter = AtomicLong()
-            ForkJoinPool(
-                THREAD_COUNT_OF_POOL, {
-                    ForkJoinPool.defaultForkJoinWorkerThreadFactory.newThread(it).apply {
-                        name = "$threadNamePrefixOfForkJoinPoolExecutor${counter.getAndIncrement()}"
-                    }
-                },
-                null, false
-            )
-        }
-
         // warmup
         val fs1: List<Future<*>> = (0 until THREAD_COUNT_OF_POOL * 2).map {
             testThreadPoolExecutor.submit { sleep() }
@@ -106,9 +104,9 @@ object InitTestThreadPoolsProjectListener : BeforeProjectListener, AfterProjectL
         testForkJoinPoolExecutor.shutdown()
 
         @Suppress("BlockingMethodInNonBlockingContext")
-        testThreadPoolExecutor.awaitTermination(1, TimeUnit.SECONDS) shouldBe true
+        testThreadPoolExecutor.awaitTermination(3, TimeUnit.SECONDS).shouldBeTrue()
         @Suppress("BlockingMethodInNonBlockingContext")
-        testForkJoinPoolExecutor.awaitTermination(1, TimeUnit.SECONDS) shouldBe true
+        testForkJoinPoolExecutor.awaitTermination(3, TimeUnit.SECONDS).shouldBeTrue()
     }
 }
 
