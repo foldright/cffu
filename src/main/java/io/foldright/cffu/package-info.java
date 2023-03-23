ddf7c1ad78ce6e8425e2582540430aca90beb5df
Jerry Lee <oldratlee@gmail.com>
2023-03-23 21:41:21 +0800

    docs: add `package-info.java` and lib description

---
+++ src/main/java/io/foldright/cffu/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * A tiny 0-dependency sidekick library for CompletableFuture
+ * to improve user experience and reduce misuse.
+ * <p>
+ * the core class is {@link io.foldright.cffu.Cffu}.
+ *
+ * @see io.foldright.cffu.Cffu
+ * @see io.foldright.cffu.CffuFactory
+ * @see io.foldright.cffu.CffuFactoryBuilder
+ */
+package io.foldright.cffu;
---
+++ src/main/java/io/foldright/cffu/tuple/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * the {@code Tuple} util classes for {@code Cffu} use.
+ *
+ * @see io.foldright.cffu.tuple.Tuple2
+ * @see io.foldright.cffu.tuple.Tuple3
+ * @see io.foldright.cffu.tuple.Tuple4
+ * @see io.foldright.cffu.tuple.Tuple5
+ * @see io.foldright.cffu.CffuFactory#cffuCombine(io.foldright.cffu.Cffu, io.foldright.cffu.Cffu)
+ */
+package io.foldright.cffu.tuple;
