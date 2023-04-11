7a8bcc45c2b58beef3b863d88768d4c825895d1c
Jerry Lee <oldratlee@gmail.com>
2023-04-12 01:34:12 +0800

    refactor `anyOfSuccess`: fast path for 1 given cf 🚅 ; add comments for one more element

--- src/main/java/io/foldright/cffu/CompletableFutureUtils.java
+++ src/main/java/io/foldright/cffu/CompletableFutureUtils.java
@@ -86,7 +86,7 @@ public final class CompletableFutureUtils {
      * when any of the given CompletableFutures success, with the same result.
      * Otherwise, all the given CompletableFutures failed, the returned CompletableFuture failed,
      * with a CompletionException holding the latest exception as its cause.
-     * If no CompletableFutures are provided, returns a completed failed CompletableFuture
+     * If no CompletableFutures are provided, returns a new CompletableFuture that is already completed exceptionally
      * with the singleton exception instance {@link #NO_CF_PROVIDED_EXCEPTION}.
      *
      * @param cfs the CompletableFutures
@@ -99,14 +99,17 @@ public final class CompletableFutureUtils {
     @SafeVarargs
     @SuppressWarnings("unchecked")
     public static <T> CompletableFuture<T> anyOfSuccess(CompletableFuture<T>... cfs) {
-        if (cfs.length == 0) return failedFuture(NO_CF_PROVIDED_EXCEPTION);
+        final int size = cfs.length;
+        if (size == 0) return failedFuture(NO_CF_PROVIDED_EXCEPTION);
+        if (size == 1) return copy(cfs[0]).toCompletableFuture();
 
         final CompletableFuture<T> incomplete = new CompletableFuture<>();
         final AtomicReference<Throwable> latestEx = new AtomicReference<>();
 
-        final CompletableFuture<T>[] successOrBeIncompleteCfs = new CompletableFuture[cfs.length + 1];
-        final CompletableFuture<T>[] failedOrBeIncompleteCfs = new CompletableFuture[cfs.length];
-        for (int i = 0; i < cfs.length; i++) {
+        // NOTE: use ONE MORE element of successOrBeIncompleteCfs LATER
+        final CompletableFuture<T>[] successOrBeIncompleteCfs = new CompletableFuture[size + 1];
+        final CompletableFuture<T>[] failedOrBeIncompleteCfs = new CompletableFuture[size];
+        for (int i = 0; i < size; i++) {
             final CompletableFuture<T> cf = cfs[i];
 
             successOrBeIncompleteCfs[i] = cf.handle((v, ex) -> ex == null ? cf : incomplete)
@@ -118,10 +121,12 @@ public final class CompletableFutureUtils {
                 return cf;
             }).thenCompose(Function.identity());
         }
+
         CompletableFuture<T> allFailed = CompletableFuture.allOf(failedOrBeIncompleteCfs)
                 .thenCompose(unused -> failedFuture(latestEx.get()));
+        // NOTE: use the ONE MORE element of successOrBeIncompleteCfs HERE
+        successOrBeIncompleteCfs[size] = allFailed;
 
-        successOrBeIncompleteCfs[cfs.length] = allFailed;
         return (CompletableFuture<T>) CompletableFuture.anyOf(successOrBeIncompleteCfs);
     }
 
@@ -258,6 +263,14 @@ public final class CompletableFutureUtils {
         final CompletableFuture<T> cf = new CompletableFuture<>();
         cf.completeExceptionally(ex);
         return cf;
+    }
+
+    @Contract(pure = true)
+    static <U> CompletableFuture<U> copy(CompletableFuture<U> cf) {
+        if (IS_JAVA9_PLUS) {
+            return cf.copy();
+        }
+        return cf.thenApply(Function.identity());
     }
 
     ////////////////////////////////////////////////////////////////////////////////
