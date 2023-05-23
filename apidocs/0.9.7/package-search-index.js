245a3f6693a4cedcb3a01ad72e8c06f284b0c263
Jerry Lee <oldratlee@gmail.com>
2023-05-24 01:00:59 +0800

    add api docs for v0.9.7

---
+++ apidocs/0.9.7/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.foldright.cffu"},{"l":"io.foldright.cffu.spi"},{"l":"io.foldright.cffu.ttl"},{"l":"io.foldright.cffu.tuple"}];updateSearchResults();
