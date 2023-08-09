477e7470fd4761c6dc9e677e50afd6af71f061a6
Jerry Lee <oldratlee@gmail.com>
2023-08-09 20:51:41 +0800

    build: fix `bump_cffu_version.sh`

--- scripts/bump_cffu_version.sh
+++ scripts/bump_cffu_version.sh
@@ -6,15 +6,16 @@ cd "$(dirname "$(readlink -f "$0")")"
 source "$PWD/bash-buddy/lib/trap_error_info.sh"
 source "$PWD/bash-buddy/lib/common_utils.sh"
 source "$PWD/bash-buddy/lib/java_utils.sh"
+source "$PWD/bash-buddy/lib/maven_utils.sh"
 
-readonly nl=$'\n' # new line
+readonly newline=$'\n'
 
 # shellcheck disable=SC2154
-[ $# -ne 1 ] && cu::die "need only 1 argument for version!$nl${nl}usage:$nl $0 x.y.z"
+[ $# -ne 1 ] && cu::die "need only 1 argument for version!$newline${newline}usage:$newline $0 x.y.z"
 readonly new_version="$1"
 
 bump_cffu_version() {
-  jvb::mvn_cmd \
+  mvu::mvn_cmd \
     org.codehaus.mojo:versions-maven-plugin:2.15.0:set \
     -DgenerateBackupPoms=false \
     -DprocessAllModules=true \
