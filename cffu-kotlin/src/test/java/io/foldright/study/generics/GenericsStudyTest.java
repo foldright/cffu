0be06a9d48dcdefeef843ac5b05c4a5d0e5bb01d
Jerry Lee <oldratlee@gmail.com>
2024-05-13 19:12:44 +0800

    WIP

---
+++ cffu-kotlin/src/test/java/io/foldright/study/generics/GenericsStudyTest.java
@@ -0,0 +1,16 @@
+package io.foldright.study.generics;
+
+import io.foldright.cffu.CompletableFutureUtils;
+import io.foldright.cffu.tuple.Tuple3;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CompletableFuture;
+
+
+/**
+ * @see io.foldright.cffu.generics.TypeParameterDeclarationTest
+ */
+@SuppressWarnings("unused")
+public class GenericsStudyTest {
+
+}
