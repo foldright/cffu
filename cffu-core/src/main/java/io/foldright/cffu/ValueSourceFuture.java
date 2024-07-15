fe9c99eaab630c19e1923cd9c8104a9c958b36e2
huhaosumail <995483610@qq.com>
2024-07-15 21:31:17 +0800

    convert ListenableFuture CompletableFuture

--- cffu-core/src/main/java/io/foldright/cffu/ValueSourceFuture.java
+++ cffu-core/src/main/java/io/foldright/cffu/ValueSourceFuture.java
@@ -3,8 +3,8 @@ package io.foldright.cffu;
 import java.util.concurrent.Future;
 
 
-public abstract class ValueSourceFuture<T> extends FutureWrapper<T> implements CompletableFutureUtils.ValueSource<T> {
+public abstract class ValueSourceFuture<T> extends FutureWrapper<T> implements ValueSource<T> {
         protected ValueSourceFuture(Future<T> wrappedFuture) {
             super(wrappedFuture);
         }
-    }
+}
