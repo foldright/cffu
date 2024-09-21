20c781ec7ca013abde66b33da7052ccbd0e2ecdf
Jerry Lee <oldratlee@gmail.com>
2024-09-21 18:27:41 +0800

    test: simplify test codes by `kotlin delegation`

--- cffu-core/src/test/java/io/foldright/cffu/CffuTestHelper.kt
+++ cffu-core/src/test/java/io/foldright/cffu/CffuTestHelper.kt
@@ -3,11 +3,15 @@
 package io.foldright.cffu
 
 import io.foldright.cffu.CffuFactoryBuilder.CffuMadeExecutor
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.CompletionStage
 import java.util.concurrent.Executor
 
 
-fun CffuFactory.unwrapMadeExecutor(): Executor = (defaultExecutor() as CffuMadeExecutor).unwrap()
+fun Cffu<*>.unwrapMadeExecutor(): Executor = defaultExecutor().unwrapMadeExecutor()
 
-fun Cffu<*>.unwrapMadeExecutor(): Executor = (defaultExecutor() as CffuMadeExecutor).unwrap()
+fun CffuFactory.unwrapMadeExecutor(): Executor = defaultExecutor().unwrapMadeExecutor()
 
 fun Executor.unwrapMadeExecutor(): Executor = (this as CffuMadeExecutor).unwrap()
+
+class FooCs<T>(cf: CompletableFuture<T>) : CompletionStage<T> by cf
