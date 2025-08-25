58f9c86616b424a914f6079a66f6f974be03e6c3
Jerry Lee <oldratlee@gmail.com>
2025-08-26 01:17:03 +0800

    docs: improve wording 📚

--- cffu-core/src/main/java/io/foldright/cffu2/BaseCffu.java
+++ cffu-core/src/main/java/io/foldright/cffu2/BaseCffu.java
@@ -1361,17 +1361,17 @@ public abstract class BaseCffu<T, F extends BaseCffu<T, F>> implements Future<T>
      * <strong>CAUTION:</strong> This method is <strong>UNSAFE</strong>!
      * <p>
      * When the wait timed out, the subsequent non-async actions of the dependent Cffus/CompletableFutures are performed
-     * in CompletableFuture's internal <strong>SINGLE-thread delay executor</strong> (including timeout functionality).
+     * in CompletableFuture's internal <strong>SINGLE-thread</strong> ScheduledThreadPoolExecutor (including timeout functionality).
      * This means that the long-running subsequent non-async actions will block this executor thread, preventing it from
      * handling other timeouts and delays, effectively breaking CompletableFuture's timeout and delay functionality.
      * <p>
      * <strong>Strongly recommend</strong> using the safe method {@link #orTimeout(long, TimeUnit) orTimeout}
      * instead of this method. Using this method is appropriate only when:
      * <ul>
-     * <li>the returned Cffu is only read explicitly(e.g. by get/join/resultNow methods), and/or
-     * <li>all subsequent actions of dependent Cffus/CompletableFutures are guaranteed to execute asynchronously
+     * <li>the returned Cffu is only read explicitly(e.g. by get/join/resultNow methods),
+     * <li>and/or all subsequent actions of dependent Cffus/CompletableFutures are guaranteed to execute asynchronously
      *    (i.e., the dependent Cffus/CompletableFutures are created using async methods).
-     * </ul> In these cases, using this unsafe method avoids an unnecessary thread switch when timeout occurs; However,
+     * </ul> In these cases, using this unsafe method avoids an unnecessary thread switching when timeout occurs; However,
      * these conditions are difficult to guarantee in practice especially when the returned Cffu is used by others' codes.
      * <p>
      * Note: Before Java 21(Java 20-), {@link CompletableFuture#orTimeout CompletableFuture#orTimeout} leaks if the future
@@ -1421,17 +1421,17 @@ public abstract class BaseCffu<T, F extends BaseCffu<T, F>> implements Future<T>
      * <strong>CAUTION:</strong> This method is <strong>UNSAFE</strong>!
      * <p>
      * When the wait timed out, the subsequent non-async actions of the dependent Cffus/CompletableFutures are performed
-     * in CompletableFuture's internal <strong>SINGLE-thread delay executor</strong> (including timeout functionality).
+     * in CompletableFuture's internal <strong>SINGLE-thread</strong> ScheduledThreadPoolExecutor (including timeout functionality).
      * This means that the long-running subsequent non-async actions will block this executor thread, preventing it from
      * handling other timeouts and delays, effectively breaking CompletableFuture's timeout and delay functionality.
      * <p>
      * <strong>Strongly recommend</strong> using the safe method {@link #completeOnTimeout(Object, long, TimeUnit)
      * completeOnTimeout} instead of this method. Using this method is appropriate only when:
      * <ul>
-     * <li>the returned Cffu is only read explicitly(e.g. by get/join/resultNow methods), and/or
-     * <li>all subsequent actions of dependent Cffus/CompletableFutures are guaranteed to execute asynchronously
+     * <li>the returned Cffu is only read explicitly(e.g. by get/join/resultNow methods),
+     * <li>and/or all subsequent actions of dependent Cffus/CompletableFutures are guaranteed to execute asynchronously
      *    (i.e., the dependent Cffus/CompletableFutures are created using async methods).
-     * </ul> In these cases, using this unsafe method avoids an unnecessary thread switch when timeout occurs; However,
+     * </ul> In these cases, using this unsafe method avoids an unnecessary thread switching when timeout occurs; However,
      * these conditions are difficult to guarantee in practice especially when the returned Cffu is used by others' codes.
      *
      * @param value   the value to use upon timeout
@@ -1789,8 +1789,8 @@ public abstract class BaseCffu<T, F extends BaseCffu<T, F>> implements Future<T>
      * will maintain its original result, regardless of whether the supplied action throws an exception or not.
      * <p>
      * <strong>CAUTION:</strong> Since this method returns the input stage directly, the execution order between
-     * the given action and other actions added to the input stage cannot be guaranteed. The action should be treated
-     * as "fire and forget" - do not make any assumptions about timing or execution sequence.
+     * the given action and other actions added to the input stage cannot be guaranteed. The given action should
+     * be treated as "fire and forget" - do not make any assumptions about timing or execution sequence.
      * <p>
      * Unlike method {@link #handle(BiFunction)} and like method {@link #whenComplete(BiConsumer)},
      * this method is not designed to translate completion outcomes.
@@ -1821,8 +1821,8 @@ public abstract class BaseCffu<T, F extends BaseCffu<T, F>> implements Future<T>
      * will maintain its original result, regardless of whether the supplied action throws an exception or not.
      * <p>
      * <strong>CAUTION:</strong> Since this method returns the input stage directly, the execution order between
-     * the given action and other actions added to the input stage cannot be guaranteed. The action should be treated
-     * as "fire and forget" - do not make any assumptions about timing or execution sequence.
+     * the given action and other actions added to the input stage cannot be guaranteed. The given action should
+     * be treated as "fire and forget" - do not make any assumptions about timing or execution sequence.
      * <p>
      * Unlike method {@link #handleAsync(BiFunction)} and like method {@link #whenCompleteAsync(BiConsumer)},
      * this method is not designed to translate completion outcomes.
@@ -1852,8 +1852,8 @@ public abstract class BaseCffu<T, F extends BaseCffu<T, F>> implements Future<T>
      * will maintain its original result, regardless of whether the supplied action throws an exception or not.
      * <p>
      * <strong>CAUTION:</strong> Since this method returns the input stage directly, the execution order between
-     * the given action and other actions added to the input stage cannot be guaranteed. The action should be treated
-     * as "fire and forget" - do not make any assumptions about timing or execution sequence.
+     * the given action and other actions added to the input stage cannot be guaranteed. The given action should
+     * be treated as "fire and forget" - do not make any assumptions about timing or execution sequence.
      * <p>
      * Unlike method {@link #handleAsync(BiFunction, Executor)} and like method {@link
      * #whenCompleteAsync(BiConsumer, Executor)}, this method is not designed to translate completion outcomes.
