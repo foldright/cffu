3ce076c12ac482f52cdeb4622eaf3fc4c1781716
Jerry Lee <oldratlee@gmail.com>
2025-11-23 16:26:42 +0800

    test: add `ExecutorInterruptionTests` ✋🧵

---
+++ cffu-core/src/test/java/io/foldright/study/executor_interruption/ExecutorInterruptionTests.kt
@@ -0,0 +1,100 @@
+package io.foldright.study.executor_interruption
+
+import io.kotest.assertions.throwables.shouldThrowExactly
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.inspectors.shouldForAll
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.throwable.shouldHaveCauseInstanceOf
+import java.lang.Thread.currentThread
+import java.lang.Thread.sleep
+import java.util.concurrent.*
+
+
+@Volatile
+private lateinit var executorThread: Thread
+private val executor: ExecutorService = Executors.newFixedThreadPool(1)
+
+class ExecutorInterruptionTests : FunSpec({
+    test("task interruption - InterruptedException") {
+        val runThreads = CopyOnWriteArrayList<Thread>()
+
+        run {
+            val submitLatch = CountDownLatch(1)
+            val f: Future<*> = executor.submit {
+                runThreads.add(currentThread())
+                submitLatch.countDown()
+                sleep(2_000)
+            }
+            // await submitted task start
+            submitLatch.await()
+
+            executorThread.interrupt()
+            sleep(50)
+
+            shouldThrowExactly<ExecutionException> { f.get() }
+                .shouldHaveCauseInstanceOf<InterruptedException>()
+            executorThread.isInterrupted.shouldBeFalse()
+
+            currentThread().isInterrupted.shouldBeFalse()
+        }
+
+        // await the second submitted task finish
+        executor.submit { runThreads.add(currentThread()) }.get().shouldBeNull()
+        // interrupted status is unset
+        currentThread().isInterrupted.shouldBeFalse()
+
+        runThreads.shouldHaveSize(2)
+        // thread is reused after interrupt (throw InterruptedException)
+        runThreads.shouldForAll { it === executorThread }
+    }
+
+    test("task interruption - isInterrupted") {
+        val runThreads = CopyOnWriteArrayList<Thread>()
+
+        run {
+            val submitLatch = CountDownLatch(1)
+            val f: Future<*> = executor.submit {
+                runThreads.add(currentThread())
+                submitLatch.countDown()
+
+                // busy loop
+                var sum: Long = 0
+                for (lng in (0..Long.MAX_VALUE)) {
+                    if (currentThread().isInterrupted) break
+                    sum += lng
+                }
+            }
+            // await submitted task start
+            submitLatch.await()
+
+            executorThread.interrupt()
+            sleep(50)
+
+            f.get().shouldBeNull()
+            // thread is reused after interrupt (set isInterrupted)
+            executorThread.isInterrupted.shouldBeFalse()
+
+            currentThread().isInterrupted.shouldBeFalse()
+        }
+
+        // await the second submitted task finish
+        executor.submit { runThreads.add(currentThread()) }.get().shouldBeNull()
+        currentThread().isInterrupted.shouldBeFalse()
+
+        runThreads.shouldHaveSize(2)
+        // reuse thread
+        runThreads.shouldForAll { it === executorThread }
+    }
+
+    beforeSpec {
+        // warmup and set executorThread
+        executor.submit({ executorThread = currentThread() }).get()
+    }
+
+    afterSpec {
+        executor.shutdownNow()
+    }
+})
+
