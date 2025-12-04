0e49c9a99e6cbfa0473b63000be8a152e2e33936
linqh <linqh@trip.com>
2025-12-04 16:01:37 +0800

    demo: interrupt propagation

--- cffu-core/src/test/java/io/foldright/study/concurrency_limit_executor/ConcurrencyLimitExecutorByLock.java
+++ cffu-core/src/test/java/io/foldright/study/concurrency_limit_executor/ConcurrencyLimitExecutorByLock.java
@@ -1,22 +1,65 @@
 package io.foldright.study.concurrency_limit_executor;
 
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFutureTask;
 import edu.umd.cs.findbugs.annotations.CheckReturnValue;
 import edu.umd.cs.findbugs.annotations.NonNull;
 
 import javax.annotation.concurrent.GuardedBy;
+import java.time.Duration;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.List;
 import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.IntStream;
 
+import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
 import static io.foldright.cffu2.internal.ExceptionLogger.Level.ERROR;
 import static io.foldright.cffu2.internal.ExceptionLogger.logUncaughtException;
 import static java.lang.Thread.currentThread;
+import static java.util.concurrent.Executors.newCachedThreadPool;
+import static java.util.concurrent.Executors.newSingleThreadScheduledExecutor;
 
 
 public final class ConcurrencyLimitExecutorByLock implements Executor {
+
+    public static void main(String[] args) {
+        ExecutorService pool = newCachedThreadPool();
+        ConcurrencyLimitExecutorByLock executor = new ConcurrencyLimitExecutorByLock(2, pool);
+
+        List<ListenableFutureTask<Integer>> tasks = IntStream.range(0, 5)
+                .mapToObj(__ -> ListenableFutureTask.create(() -> {
+                    if (currentThread().isInterrupted()) {
+                        return -1;
+                    }
+                    try {
+                        Thread.sleep(Duration.ofSeconds(3));
+                    } catch (InterruptedException ex) {
+                        currentThread().interrupt();
+                        return 0;
+                    }
+                    return 1;
+                }))
+                .toList();
+        tasks.forEach(executor::execute);
+
+        Futures.withTimeout(Futures.allAsList(tasks), 2, TimeUnit.SECONDS, newSingleThreadScheduledExecutor());
+        Futures.whenAllComplete(tasks)
+                .run(() -> tasks.forEach(t -> {
+                    switch (t.state()) {
+                        case SUCCESS -> System.out.println("ok: " + t.resultNow());
+                        case FAILED -> System.out.println("err" + t.exceptionNow().getMessage());
+                        case CANCELLED -> System.out.println("canceled");
+                        case RUNNING -> throw new AssertionError();
+                    }
+                }), directExecutor());
+    }
+
     private final int maxConcurrency;
     private final Executor executor;
 
